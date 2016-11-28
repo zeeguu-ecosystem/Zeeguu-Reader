210570f28fb7c2266e9f4c50e2d86128670722d3
Luc van den Brand <lucas.brnd@gmail.com>
2016-11-28 14:04:54 +0100

    Setup for translation insertion code.

--- static/scripts/translation.js
+++ static/scripts/translation.js
@@ -1,12 +1,39 @@
- $(document).click(function(){
-        var s = window.getSelection();
-        s.modify('extend','backward','word');        
-        var b = s.toString();
+var ZEEGUU_SERVER = "https://www.zeeguu.unibe.ch";
+var HTML_ZEEGUUTAG = "ZEEGUU";
+ 
+$(document).click(function()
+{
+	tagText();
+});
+ 
+function tagText() 
+{
+	// Select content.
+	var selection = window.getSelection();
+	selection.modify('move','backward','word');
+	selection.modify('extend','forward','word');  
+	
+	// Insert tags.
+	var range = selection.getRangeAt(0);
+	var zeeguuTag = document.createElement(HTML_ZEEGUUTAG);
+	range.surroundContents(zeeguuTag);
+	
+	// Launch request.
+	var text = selection.toString();
+	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            setTranslation(zeeguuTag, xmlHttp.responseText);
+    }
+    // TODO : Make the below work.
+    var postData = "context="+text;
+    postData += "&url=zeeguu-mr-core.herokuapp.com"
+    postData += "&word="+text;
+    xmlHttp.open("POST", ZEEGUU_SERVER+"/translate/nl/en", true); 
+    xmlHttp.send(postData);
+}
 
-        s.modify('extend','forward','word');
-        var a = s.toString();
-        s.modify('move','forward','character');
-        var selection = b+a;
-        console.log(selection+'\nSessionID:'+sessionID);
- });
-
+function setTranslation(zeeguuTag, translation)
+{	
+	zeeguuTag.innerHTML = "hi";
+}
