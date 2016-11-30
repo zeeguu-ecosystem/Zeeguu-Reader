1482358a2051e234bc4be22b33880a12cb73d2e1
Lukeslux <Lucas.brnd@gmail.com>
2016-11-30 14:15:51 +0100

    Fixed basic translation feature.

--- static/scripts/translation.js
+++ static/scripts/translation.js
@@ -11,29 +11,35 @@ function tagText()
 	// Select content.
 	var selection = window.getSelection();
 	selection.modify('move','backward','word');
-	selection.modify('extend','forward','word');  
-	
-	// Insert tags.
+	selection.modify('extend','forward','word'); 
+	var text = selection.toString(); 
 	var range = selection.getRangeAt(0);
-	var zeeguuTag = document.createElement(HTML_ZEEGUUTAG);
-	range.surroundContents(zeeguuTag);
 	
-	// Launch request.
-	var text = selection.toString();
-	var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            setTranslation(zeeguuTag, xmlHttp.responseText);
-    }
-    // TODO : Make the below work.
-    var postData = "context="+text;
-    postData += "&url=zeeguu-mr-core.herokuapp.com"
-    postData += "&word="+text;
-    xmlHttp.open("POST", ZEEGUU_SERVER+"/translate/nl/en", true); 
-    xmlHttp.send(postData);
+	// Check if selection has been translated already.
+	if (range.commonAncestorContainer.parentNode.nodeName != HTML_ZEEGUUTAG) 
+	{
+		// Insert tags.
+		var zeeguuTag = document.createElement(HTML_ZEEGUUTAG);
+		range.surroundContents(zeeguuTag);
+		
+		// Launch request.
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() { 
+			if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+				setTranslation(zeeguuTag, xmlHttp.responseText);
+		}
+		var postData = "context="+text;
+		postData += "&url=zeeguu-mr-core.herokuapp.com"
+		postData += "&word="+text;
+		//console.log(postData);
+		xmlHttp.open("POST", ZEEGUU_SERVER+"/translate/nl/en", true); 
+		xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlHttp.send(postData);
+	}
+    selection.modify('move','backward','character');
 }
 
 function setTranslation(zeeguuTag, translation)
 {	
-	zeeguuTag.innerHTML = "hi";
+	zeeguuTag.innerHTML += " ("+translation+")";
 }
