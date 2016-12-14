1cf0432a827acfc1454ccbb7a190e2ef057de933
Lukeslux <Lucas.brnd@gmail.com>
2016-12-14 15:45:08 +0100

    Fixed last sentence translate bug, added consecutive word merging.

--- static/scripts/translation.js
+++ static/scripts/translation.js
@@ -5,20 +5,30 @@ var SENTMODE = "sentence";
 var translateMode = WORDMODE;
  
 $(document).ready(function() {
-	$(".translatable").click(function() {
-		tagText();
-		if (translateMode == SENTMODE)
-		    document.getElementById("modeSwitch").click();
-	});
-	
-	$('#modeSwitch').click(function(){
-		if($(this).is(':checked')){
-			translateMode = SENTMODE;
-		} else {
-			translateMode = WORDMODE;
+	$(".translatable").click(function() 
+	{
+		if (isText(window.getSelection()))
+		{
+			tagText();
+			if (translateMode == SENTMODE)
+				document.getElementById("modeSwitch").click();
 		}
 	});
+	
+	$('#modeSwitch').click(function()
+	{
+		if($(this).is(':checked'))
+			translateMode = SENTMODE;
+		else 
+			translateMode = WORDMODE;
+	});
 });
+
+function isText(selection)
+{
+	return true;
+}
+
  
 // Wraps a zeeguutag including translation around the selected content.
 function tagText() 
@@ -39,34 +49,34 @@ function tagText()
 	coverTag(selection, HTML_ZEEGUUTAG);
 	
 	// Check if selection has been tagged already.
-	if (isTagged(selection, HTML_ZEEGUUTAG)) {
-		selection.modify('move','backward','character');
-		return;
+	if (!(range.commonAncestorContainer.parentNode.nodeName == HTML_ZEEGUUTAG) 
+	     && !isTagged(selection, HTML_ZEEGUUTAG)) {
+		// Insert tags.
+		range = selection.getRangeAt(0);	
+		
+		var zeeguuTag = document.createElement(HTML_ZEEGUUTAG);
+		var contents = range.extractContents();
+		clearTags(contents, HTML_ZEEGUUTAG);
+		zeeguuTag.appendChild(contents);
+		range.insertNode(zeeguuTag);
+		
+		mergeZeeguu(zeeguuTag);
+		
+		// Launch request to Zeeguu API.
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() { 
+			if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+				setTranslation(zeeguuTag, xmlHttp.responseText);
+		}
+		
+		var text = escape(zeeguuTag.textContent); 
+		var postData = "context="+text;
+		postData += "&url=zeeguu-mr-core.herokuapp.com"
+		postData += "&word="+text;
+		xmlHttp.open("POST", ZEEGUU_SERVER+"/translate/nl/en", true); 
+		xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlHttp.send(postData);
 	}
-	
-	// Insert tags.
-	var text = escape(selection.toString()); 
-	range = selection.getRangeAt(0);	
-	
-	var zeeguuTag = document.createElement(HTML_ZEEGUUTAG);
-	var contents = range.extractContents();
-    clearTags(contents, HTML_ZEEGUUTAG);
-	zeeguuTag.appendChild(contents);
-	range.insertNode(zeeguuTag);
-	
-	// Launch request to Zeeguu API.
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function() { 
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-			setTranslation(zeeguuTag, xmlHttp.responseText);
-	}
-	
-	var postData = "context="+text;
-	postData += "&url=zeeguu-mr-core.herokuapp.com"
-	postData += "&word="+text;
-	xmlHttp.open("POST", ZEEGUU_SERVER+"/translate/nl/en", true); 
-	xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xmlHttp.send(postData);
 	
     selection.modify('move','backward','character');
 }
@@ -74,6 +84,35 @@ function tagText()
 function setTranslation(zeeguuTag, translation)
 {	
 	zeeguuTag.setAttribute("translation", translation);
+}
+
+function mergeZeeguu(zeeguuTag)
+{
+	var spaces = '';
+	var node = zeeguuTag.previousSibling;
+	while (node.textContent == ' ')
+	{
+		node = node.previousSibling;
+		spaces += ' ';
+	}
+	if (node.nodeName == HTML_ZEEGUUTAG)
+	{
+		zeeguuTag.textContent = node.textContent + spaces + zeeguuTag.textContent;
+		node.parentNode.removeChild(node);
+	}
+	spaces = '';
+    node = zeeguuTag.nextSibling;
+	while (node.textContent == ' ')
+	{
+		node = node.nextSibling;
+		spaces += ' ';
+		
+	}
+	if (node.nodeName == HTML_ZEEGUUTAG)
+	{
+		zeeguuTag.textContent += spaces + node.textContent;
+		node.parentNode.removeChild(node);
+	}
 }
 
 // Fixes partial selection of 'tag' nodes.
@@ -86,6 +125,7 @@ function coverTag(selection, tag)
 	selection.addRange(range);
 }
 
+// Checks if the selection is tagged with tag.
 function isTagged(selection, tag) {
 	var range = selection.getRangeAt(0);
 	var contents = range.cloneContents();
