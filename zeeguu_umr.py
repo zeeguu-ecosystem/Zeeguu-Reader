5da8ba96640ec613274e2f5fccc9c52debc6d647
Luc van den Brand <lucas.brnd@gmail.com>
2017-02-14 12:30:22 +0100

    Removed redundant zeeguuRequests.py

--- zeeguu_umr.py
+++ zeeguu_umr.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, make_response, send_from_directory
-from feed import getFeed;
-from zeeguuRequests import ZEEGUU_SERVER, STATUS_ACCEPT
 import article
 import requests
 import os
+
+ZEEGUU_SERVER = "https://www.zeeguu.unibe.ch"
+STATUS_WRONGUSER  = 401; STATUS_WRONGPASS = 400; STATUS_ACCEPT = 200;
 
 app = Flask(__name__)
 
@@ -11,11 +12,12 @@ app = Flask(__name__)
 def getFavicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'images/favicon.ico')
 
-# Main entrypoint, asks the user to login before continuing.       
+# Main entrypoint, asks the user to login before continuing.
 @app.route('/', methods=['GET', 'POST'])
 def handleEntry():
     if 'sessionID' in request.cookies:
-        return getFeed(request.cookies.get('sessionID'))
+        sessionID = request.cookies.get('sessionID')
+        return getFeedList(sessionID)
     else:
         if request.method == 'POST':
             return handleLoginForm()
@@ -31,15 +33,19 @@ def handleLoginForm():
     username = request.form['username']
     password = {'password' : request.form['password']}
     result = requests.post(ZEEGUU_SERVER+'/session/'+username, password)
-    
+
     # Check for login succces, sends the user back to login or continues.
     if (result.status_code == STATUS_ACCEPT):
         sessionID = result.content
-        response = make_response(getFeed(sessionID))
+        response = make_response(getFeedList(sessionID))
         response.set_cookie('sessionID', sessionID)
     else:
 	response = make_response(getLoginForm())
     return response;
+
+# Return the main page where the articles and feeds are listed.
+def getFeedList(sessionID):
+    return render_template('feedlist.html', sessionID=sessionID)
 
 # Returns a zeeguu enhanced article.
 @app.route('/article', methods=['GET'])
