6d3424aea164586097f8823c588b982a853a4d1e
Lukeslux <Lucas.brnd@gmail.com>
2017-03-22 12:29:27 +0100

    All Python variable names are now in lower case.

--- zeeguu_umr.py
+++ zeeguu_umr.py
@@ -4,7 +4,7 @@ import requests
 import os
 
 ZEEGUU_SERVER = "https://www.zeeguu.unibe.ch/api"
-STATUS_ACCEPT = 200;
+STATUS_ACCEPT = 200
 
 app = Flask(__name__)
 
@@ -18,8 +18,8 @@ def get_favicon():
 @app.route('/', methods=['GET', 'POST'])
 def handle_entry():
     if 'sessionID' in request.cookies:
-        sessionID = request.cookies.get('sessionID')
-        return get_feed_page(sessionID)
+        session = request.cookies.get('sessionID')
+        return get_feed_page(session)
     else:
         if request.method == 'POST':
             return handle_login_form()
@@ -35,30 +35,30 @@ def get_login_form():
 # Handle login request.
 def handle_login_form():
     username = request.form['username']
-    password = {'password' : request.form['password']}
+    password = {'password': request.form['password']}
     result = requests.post(ZEEGUU_SERVER+'/session/'+username, password)
 
     # Check for login success, sends the user back to login or continues.
     if result.status_code == STATUS_ACCEPT:
-        sessionID = result.content
-        response = make_response(get_feed_page(sessionID))
-        response.set_cookie('sessionID', sessionID)
+        session = result.content
+        response = make_response(get_feed_page(session))
+        response.set_cookie('sessionID', session)
     else:
         response = make_response(get_login_form())
-    return response;
+    return response
 
 
 # Return the main page where the articles and feeds are listed.
-def get_feed_page(sessionID):
-    return render_template('feedlist.html', sessionID=sessionID)
+def get_feed_page(session):
+    return render_template('feedlist.html', sessionID=session)
 
 
 # Returns a zeeguu enhanced article.
 @app.route('/article/', methods=['POST'])
 def get_article():
-    sessionID  = request.form['sessionID']
-    articleURL = request.form['articleURL']
-    articleLanguage = request.form['articleLanguage']
-    response = requests.get(articleURL)
-    print "User with session " + sessionID + " retrieved " + articleURL;
-    return article.make_article(sessionID, response.text, articleLanguage)
+    session = request.form['sessionID']
+    article_url = request.form['articleURL']
+    article_language = request.form['articleLanguage']
+    response = requests.get(article_url)
+    print "User with session " + session + " retrieved " + article_url
+    return article.make_article(session, response.text, article_language)
