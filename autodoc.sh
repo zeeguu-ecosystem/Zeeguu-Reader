e2de75574ddbb4f82c634aa8eaeb753f344cd4fd
Lukeslux <lucas.brnd@gmail.com>
2017-05-02 21:36:25 +0200

    Allows for non-master documentation.

--- autodoc.sh
+++ autodoc.sh
@@ -4,23 +4,23 @@
 # Expects esdoc and git to be setup.
 
 if [ "$(id -u)" == "0" ]; then
-   echo "This script should not be run as root. " 1>&2
+   echo "This script should not be run as root!" 1>&2
    exit 1
 fi
 
 if [[ -n $(git status --porcelain) ]]
 then
-	echo "Repository is not clean!"
+	echo "Repository should be clean!"
 	exit 1
 fi
 
-TOP=$(cd $(dirname $0) && pwd -L)
+DATE=`date +%Y-%m-%d:%H:%M:%S`
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 # Generate documentation.
-if [[ "$BRANCH" = "development_core" ]]
+if [[ "$BRANCH" = "master" ]]
 then
-	echo "Making gh-pages documentation"
+	echo "On master => Pushing documentation to gh-pages"
 	git checkout gh-pages
 	git merge $BRANCH --no-commit --no-ff
 	git reset HEAD
@@ -29,10 +29,13 @@ then
 	mv -v ./doc/dist/* .
 	rm doc -r
 	git add --all
-	DATE=`date +%Y-%m-%d:%H:%M:%S`
 	git commit -m "AUTODOC: Documentation updated on $DATE"
 	git push
 	git checkout $BRANCH
 else
-	echo "Not development, not building documentation."
+	echo "Not master => Pushing local documentation to $BRANCH."
+	esdoc
+	git add --all
+	git commit -m "AUTODOC: Documentation updated on $DATE"
+	git push
 fi
