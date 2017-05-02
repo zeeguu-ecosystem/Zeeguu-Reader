410013eb911ec1b8f259db3d73c8c5fc47f62703
Lukeslux <Lucas.brnd@gmail.com>
2017-05-02 13:17:27 +0200

    Autodoc script defined.

---
+++ document.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Run this script to document the javascript code.
+# Be sure your repository is clean and ready before doing this!
+# Expects esdoc and git to be setup.
+
+if [ "$(id -u)" == "0" ]; then
+   echo "This script should not be run as root. " 1>&2
+   exit 1
+fi
+
+TOP=$(cd $(dirname $0) && pwd -L)
+
+# Generate documentation.
+git checkout gh-pages
+git merge development_core --no-commit --no-ff
+esdoc
+ls -A | grep -v '^doc$\|^document.sh$' | xargs rm -r
+mv -v ./doc/dist/* .
+rm doc -r
+rm document.sh
