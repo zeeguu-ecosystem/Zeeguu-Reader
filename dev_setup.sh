20aacc3031fbe544d6508d4228b207716cabcf0e
Lukeslux <Lucas.brnd@gmail.com>
2017-05-03 14:34:49 +0200

    Fixed tiny mistakes.

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 # Run this script to setup your development environment.
 
+TOP=$(cd $(dirname $0) && pwd -L)
+VIRTUALENV_ROOT=${VIRTUALENV_ROOT:-"${HOME}/.virtualenvs/zeeguu_umr"}
+
 if [ "$(id -u)" == "0" ]; then
    echo "This script should not be run as root. " 1>&2
    exit 1
 fi
+
+echo "Hello developer! Let's get you setup! :)"
 
 # Required packages.
 read -p "Install dependencies with apt? Will need root priviliges." -n 1 -r
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    echo "Apt-get installing..."
     sudo apt-get install -y \
     python \
     python-dev \
@@ -23,13 +29,12 @@ then
     sudo apt-get install nodejs
     sudo npm install -g webpack
     sudo npm install -g esdoc
-    sudo npm install
+else
+    echo "Skipping apt-get install..."
 fi
 
-TOP=$(cd $(dirname $0) && pwd -L)
-VIRTUALENV_ROOT=${VIRTUALENV_ROOT:-"${HOME}/.virtualenvs/zeeguu_umr"}
-
 # create virtualenv, consistent with virtualenv-wrapper conventions
+echo "Setting up virtual environment..."
 if [ ! -d ${VIRTUALENV_ROOT} ]; then
    mkdir -p $(dirname ${VIRTUALENV_ROOT})
   virtualenv -p python2.7 ${VIRTUALENV_ROOT}
@@ -39,3 +44,6 @@ cd ${TOP}
 
 # install requirements
 pip install -r requirements.txt 
+npm install
+
+echo "Done!"
