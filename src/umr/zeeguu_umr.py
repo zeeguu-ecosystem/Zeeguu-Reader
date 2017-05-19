4148dbf589fff01aca8a8449954106d0e0a67cc2
Mircea Lungu <mircea.lungu@gmail.com>
2017-05-19 19:42:04 +0200

    .

--- src/umr/zeeguu_umr.py
+++ src/umr/zeeguu_umr.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask, send_from_directory
-from umr.login import endpoints_login
+# from .login import endpoints_login
 from umr import umrblue
 
 app = Flask(__name__)
-app.register_blueprint(endpoints_login)
+# app.register_blueprint(endpoints_login)
 app.register_blueprint(umrblue)
 
 
