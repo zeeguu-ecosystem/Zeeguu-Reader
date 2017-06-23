44f0ce042238278e7273020839841d185a2cc2d5
Lukeslux <Lucas.brnd@gmail.com>
2017-06-23 15:14:03 +0200

    Updated version.

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="umr",
-    version="0.7",
+    version="0.7.5",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
