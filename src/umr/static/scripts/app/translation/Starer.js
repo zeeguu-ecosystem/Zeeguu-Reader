75708c626cc18de92d2b23417ece0d72e0dc579e
Mircea Lungu <mircea.lungu@gmail.com>
2018-03-10 18:50:07 +0100

    migrated to the latest version of the API

--- src/umr/static/scripts/app/translation/Starer.js
+++ src/umr/static/scripts/app/translation/Starer.js
@@ -2,8 +2,7 @@ import $ from 'jquery';
 import config from '../config';
 import UserActivityLogger from '../UserActivityLogger';
 import ZeeguuRequests from '../zeeguuRequests';
-import {POST_UNSTAR_ARTICLE} from '../zeeguuRequests';
-import {POST_STAR_ARTICLE} from '../zeeguuRequests';
+import {POST_USER_ARTICLE} from '../zeeguuRequests';
 
 
 const USER_EVENT_STAR_ARTICLE = 'STAR ARTICLE';
@@ -13,20 +12,20 @@ const OFF = 'off';
 const HTML_ID_TOGGLESTAR = '#toggle_star';
 
 /**
- * Implements the functionality for starring an article, together with 
+ * Implements the functionality for starring an article, together with
  * notifying Zeeguu about the changes.
  */
 export default class Starer {
     /**
      * Initializes the state of the starer (default is false).
      */
-    constructor(state=false) {
-        this.on = state; 
+    constructor(state = false) {
+        this.on = state;
     }
 
     /**
      * Sets the state of the starer.
-     * @param {boolean} state - Defines the state for the starer: true - 'on' or false - 'off' 
+     * @param {boolean} state - Defines the state for the starer: true - 'on' or false - 'off'
      */
     setState(state) {
         this.on = state;
@@ -42,12 +41,13 @@ export default class Starer {
 
         if (this.on) {
             // Launch Zeeguu request to notify about unstarring of article by user.
-            ZeeguuRequests.post(POST_UNSTAR_ARTICLE, {url: url});
+            // ZeeguuRequests.post(POST_UNSTAR_ARTICLE, {url: url});
+            ZeeguuRequests.post(POST_USER_ARTICLE, {url: url, starred: "False"});
             UserActivityLogger.log(USER_EVENT_UNSTAR_ARTICLE, url);
-    
+
         } else { // it's off            
             // Launch Zeeguu request to notify about starring an article.
-            ZeeguuRequests.post(POST_STAR_ARTICLE, {url: url});
+            ZeeguuRequests.post(POST_USER_ARTICLE, {url: url, starred: "True"});
             UserActivityLogger.log(USER_EVENT_STAR_ARTICLE, url);
         }
         this._toggleState();
@@ -65,7 +65,7 @@ export default class Starer {
      * Toggles the icon of the star by adding or removing a class representing OFF.
      */
     _toggleIcon() {
-        $(HTML_ID_TOGGLESTAR).children().each(function() {
+        $(HTML_ID_TOGGLESTAR).children().each(function () {
             $(this).toggleClass(OFF);
         });
     }
