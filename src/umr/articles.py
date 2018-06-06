c1229418302da2bfad23dec991e08db739f9d611
Mircea Lungu <mircea.lungu@gmail.com>
2018-06-06 18:54:21 +0200

    no need for redirect

--- src/umr/articles.py
+++ src/umr/articles.py
@@ -7,9 +7,5 @@ from .session import with_session
 @with_session
 def articles():
     """Return the main page where the articles and feeds are listed."""
-    return get_articles_page()
 
-
-def get_articles_page():
-    """Return the template that shows all available articles and feeds."""
-    return render_template('articles.html')
+    return render_template('articles.html',)
