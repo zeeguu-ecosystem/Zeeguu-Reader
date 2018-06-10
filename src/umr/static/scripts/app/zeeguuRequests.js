10321fcda268593f370a5d3a5bfd3f192652b5ed
feikoritsema <feiko.ritsema@hotmail.com>
2018-06-10 12:37:21 +0200

    better redeployment indeed

--- src/umr/static/scripts/app/zeeguuRequests.js
+++ src/umr/static/scripts/app/zeeguuRequests.js
@@ -1,8 +1,5 @@
 import $ from 'jquery';
-/* Deployment server:
-const ZEEGUU_SERVER = 'https://zeeguu.unibe.ch/api'; */
-/* Test server: */
-const ZEEGUU_SERVER = 'http://localhost:9001';
+const ZEEGUU_SERVER = 'https://zeeguu.unibe.ch/api';
 const ZEEGUU_SESSION = 'sessionID';
 
 /** Get a list of recommended feeds. */
