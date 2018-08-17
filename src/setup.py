6b3645188b50553a135aa128495a97638f56f26b
Mircea Lungu <mircea.lungu@gmail.com>
2018-08-17 11:12:56 +0200

    hid the article sources. it's not used anymore in the recommender

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="umr",
-    version="0.9.127",
+    version="0.9.128",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
