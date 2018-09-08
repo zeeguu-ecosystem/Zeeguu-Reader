d3ffef28879028d187c3ad3aa7983e79765cc118
Mircea Lungu <mircea.lungu@gmail.com>
2018-09-08 15:10:47 +0200

    added link to review vocabulary

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="umr",
-    version="0.9.143",
+    version="0.9.144",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
