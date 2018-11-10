d121d58ca4c107789e01c9c33d6060542ddc623e
Mircea Lungu <mircea.lungu@gmail.com>
2018-11-10 17:38:56 +0100

    cleaned up the stylesheets

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="umr",
-    version="0.9.294",
+    version="0.9.333",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
