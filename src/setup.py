567c616e70e3216cfc91c6144b6b5fe0770c759b
Mircea Lungu <mircea.lungu@gmail.com>
2020-02-14 12:50:13 +0100

    updated package dependencies

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="umr",
-    version="0.9.353",
+    version="0.9.354",
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
